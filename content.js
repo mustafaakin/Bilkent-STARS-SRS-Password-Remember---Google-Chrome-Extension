651f22d4a747eea78f0b3c145650842506306296
Mustafa Akin <mustafa91@gmail.com>
2011-06-29 19:58:50 +0300

    Init

---
+++ content.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function(){
+	var u  = localStorage["user"];
+	var p  = localStorage["pwd"];
+	if ( u != undefined && p != undefined){
+		$("#ID").val(u).css("background-color","yellow");
+		$("#PWD").val(p).css("background-color","yellow");
+	}
+	$(".actionbutton").click( function(){
+		if ( $(this).attr("type") == "submit" && $(this).attr("Value") == "Login"){
+			var a = $("#ID").val();
+			var b = $("#PWD").val();
+			localStorage["user"] = a;
+			localStorage["pwd"] = b;
+		}
+	});
+});
